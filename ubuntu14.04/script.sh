e9b5fa1d0a76d6a38a6575d51b01baa69fe4c90d
geekduck <duck1218+github@gmail.com>
2014-12-24 23:14:21 +0900

    Set locale and timezone in init script.

--- ubuntu14.04/script.sh
+++ ubuntu14.04/script.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
+# set locale
+sudo locale-gen ja_JP.UTF-8
+sudo dpkg-reconfigure locales
+
+# set timezone
+sudo sh -c "echo 'Asia/Tokyo' > /etc/timezone"
+sudo dpkg-reconfigure -f noninteractive tzdata
+
 sudo apt-get update
 #sudo apt-get upgrade -y
 #sudo apt-get dist-upgrade -y
-
-sudo locale-gen ja_JP.UTF-8
