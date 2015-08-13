21253fe3d96f6e9596c1b47584174ff4048137cb
geekduck <duck1218+github@gmail.com>
2015-08-13 23:35:58 +0900

    Add multiple template.

--- aws-amazonlinux/script.sh
+++ aws-amazonlinux/script.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
-
+sudo chkconfig iptables off
+sudo service iptables stop
+#sudo yum --enablerepo=epel,rpmforge,remi update -y
 sudo yum update -y
+sudo yum install -y rsync
+#sudo yum install -y yum-utils
+#sudo package-cleanup --oldkernels
