fad8a6bd6a7a81c50c5d67ed4403b5189b26e943
geekduck <duck1218+github@gmail.com>
2015-09-12 01:20:20 +0900

    Add noninteractive.

--- debian/script.sh
+++ debian/script.sh
@@ -2,7 +2,7 @@
 
 # set locale
 sudo /usr/sbin/locale-gen ja_JP.UTF-8
-sudo /usr/sbin/dpkg-reconfigure locales
+sudo /usr/sbin/dpkg-reconfigure -f noninteractive locales
 
 # set timezone
 sudo sh -c "echo 'Asia/Tokyo' > /etc/timezone"
