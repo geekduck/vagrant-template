3807e66db4d29d782ca2db03c9bf7334581e5ecb
geekduck <duck1218+github@gmail.com>
2016-08-30 23:48:37 +0900

    install python2

--- ubuntu16.04/script.sh
+++ ubuntu16.04/script.sh
@@ -9,7 +9,4 @@ sudo sh -c "echo 'Asia/Tokyo' > /etc/timezone"
 sudo dpkg-reconfigure -f noninteractive tzdata
 
 sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get dist-upgrade -y
-
-#sudo apt-get install -y git tmux emacs24-nox tree unzip curl wget colordiff golang
+sudo apt-get install -y python
